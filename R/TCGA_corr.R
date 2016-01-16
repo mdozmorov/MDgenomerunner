21bd55b7211b868f3c9a51b0495899a754b8cca5
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-01-16 15:55:08 -0500

    Docs update, and typo fix

--- R/TCGA_corr.R
+++ R/TCGA_corr.R
@@ -3,8 +3,12 @@
 #' A function to get Pearson correlation coefficients between RSEM gene expression of
 #' a gene of interest and all other genes in a specific cancer type.
 #' 
-#' @param cancer - cancer type abbreviation. Required. Example: "BRCA". Look up
-#' abbreviations \url{http://www.liuzlab.org/TCGA2STAT/CancerDataChecklist.pdf}
+#' @param cancer - cancer type abbreviation. Required. Example: "BRCA". One of
+#' c("ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD", "COADREAD", "DLBC", "ESCA", "FPPP", 
+#' "GBM", "GBMLGG", "HNSC", "KICH", "KIPAN", "KIRC", "KIRP", "LAML", "LGG", "LIHC", 
+#' "LUAD", "LUSC", "MESO", "OV", "PAAD", "PCPG", "PRAD", "READ", "SARC", "SKCM", "STAD", 
+#' "TGCT", "THCA", "THYM", "UCEC", "UCS", "UVM"). Look up
+#' abbreviations at \url{http://www.liuzlab.org/TCGA2STAT/CancerDataChecklist.pdf}
 #' and \url{https://tcga-data.nci.nih.gov/datareports/codeTablesReport.htm}
 #' @param gene gene of interest. Required. Example: "BRCA1"
 #'
@@ -12,11 +16,22 @@
 #' "hgnc_symbol", "cor", "pval", "description" 
 #' @export
 #' @examples
+#' 
 #' \dontrun{
-#' BRCA1_corr <- TCGA_corr(canter = "BRCA", gene = "BRCA1")
+#' 
+#' # Get correlation matrixes for the gene of interest in all cancers
+#' cancers <- c("ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD", "COADREAD", "DLBC", "ESCA", "FPPP", "GBM", "GBMLGG", "HNSC", "KICH", "KIPAN", "KIRC", "KIRP", "LAML", "LGG", "LIHC", "LUAD", "LUSC", "MESO", "OV", "PAAD", "PCPG", "PRAD", "READ", "SARC", "SKCM", "STAD", "TGCT", "THCA", "THYM", "UCEC", "UCS", "UVM")
+#' gene <- "FLI1"
+#' 
+#' for (i in cancers) {
+#'   print(i)
+#'   gene_corr <- TCGA_corr(cancer = i, gene = gene)
+#'   write.table(gene_corr, paste0("TCGA_corr_", gene, "_", i, ".txt"), sep = "\t", quote = F, row.names = F)
+#' }
+#' 
 #' }
 ##
-TCGA_corr <- function(canter = "BRCA", gene = "BRCA1") {
+TCGA_corr <- function(cancer = "BRCA", gene = "BRCA1") {
   # Retrieve the data
   data.tcga <- TCGA2STAT::getTCGA(disease = cancer, data.type = "RNASeq2", type="rsem", clinical = TRUE)
   # Check if the gene of interest in the fetched data
