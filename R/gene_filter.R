6ab2a68f53f2a0d5d92fe054951881d4f1ca321f
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-09-17 21:11:08 -0400

    Explicit genefilter call

--- R/gene_filter.R
+++ R/gene_filter.R
@@ -27,7 +27,7 @@
 gene_filter <- function(mtx, proportion = 0.33, average_expression = 10, min_difference = 2) {
   f1 <- genefilter::pOverA(proportion, average_expression) # A function to calculate the proportion of genes expressed above average_expression
   f2 <- function(x) (diff(range(x, na.rm=T)) > min_difference) # A function to calculate the difference
-  ff <- filterfun(f1,f2) # A combined function
+  ff <- genefilter::filterfun(f1,f2) # A combined function
   index <- genefilter::genefilter(mtx, ff) # Filter a dataset
   return(mtx[index, ])
 }
