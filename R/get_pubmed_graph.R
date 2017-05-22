a81e5c6d11c147c37e48548013e8168e302df9bc
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2017-05-21 20:44:19 -0400

    Add customizable axes labels

--- R/get_pubmed_graph.R
+++ R/get_pubmed_graph.R
@@ -9,6 +9,8 @@
 #' @param yearend last year to search the term. Required. Default: 2016
 #' @param normalize whether to normalize term counts by the total number
 #' of published papers per year (returns proportion of the term out of the total). Default - FALSE
+#' @param xlab labels for X axis. Default - "X axis"
+#' @param ylab labels for Y axis. Default - "Y axis"
 #' @param id what type of ID is provided. "symbol" (e.g., "BRCA1") or "entrezid" (e.g., "672", default and recommended).
 #'
 #' @return a ggplot object, and plot the barplot
@@ -18,7 +20,7 @@
 #' get_pubmed_graph("retraction", yearstart = 2000, yearend = 2016)
 #' }
 ##
-get_pubmed_graph <- function(term, yearstart = 1970, yearend = 2016, normalize = FALSE) {
+get_pubmed_graph <- function(term, yearstart = 1970, yearend = 2016, normalize = FALSE, xlab = "X axis", ylab = "Y axis") {
   # http://davetang.org/muse/2013/10/31/querying-pubmed-using-r/
   #In order not to overload the E-utility servers, NCBI recommends that users post no more than three
   #URL requests per second and limit large jobs to either weekends or between 9:00 PM and 5:00 AM
@@ -42,10 +44,11 @@ get_pubmed_graph <- function(term, yearstart = 1970, yearend = 2016, normalize =
   names(tally) <- yearstart:yearend
   # barplot(tally, las=2, ylim=c(0, max(tally)), main=paste0("Number of PubMed articles containing '", term, "'"))
   tally.df <- data.frame(year=names(tally), counts=tally)
-  ggplot2::ggplot(tally.df, aes(x=factor(year), y=counts)) + 
+  p=ggplot2::ggplot(tally.df, aes(x=factor(year), y=counts)) + 
     geom_bar(stat = "identity", fill="darkblue") + 
-    scale_y_continuous(paste0("Number of PubMed articles containing '", term, "'")) + 
-    scale_x_discrete("Year") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#     scale_y_continuous(paste0("Number of PubMed articles containing '", term, "'")) + 
+#     scale_x_discrete("Year") + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    xlab(xlab) + ylab(ylab)
   #return(tally.df)
 }
